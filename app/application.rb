2b4ab5e17eaa475f8a7f8a5fb27166df7f902d10
Ryan Manchester <ryanm17@gmail.com>
2018-05-22 00:46:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,11 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      binding.pry
+      #binding.pry
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
-      
+
     else
       resp.write "Route not found"
       resp.status = 404
