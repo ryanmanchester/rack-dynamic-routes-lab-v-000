fccdce8382e19eaa20fb0015954989b57dd00910
Ryan Manchester <ryanm17@gmail.com>
2018-05-22 01:06:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
 
-      if !item.include?(item_name)
+      if item == nil
         resp.write "Item not found"
         resp.status = 400
       else
