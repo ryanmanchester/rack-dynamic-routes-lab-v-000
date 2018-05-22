0bc062e4616ab1d18cfc26a9a58c5e1ab6dd1137
Ryan Manchester <ryanm17@gmail.com>
2018-05-22 01:03:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,10 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
+      if item == nil
+        resp.write "Item not found"
+        resp.status = 400
+      end
     binding.pry
 
 
