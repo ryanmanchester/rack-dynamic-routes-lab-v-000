96324101f6ad83a6986bfc938b74da0d7bc5c597
Ryan Manchester <ryanm17@gmail.com>
2018-05-22 01:03:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,9 +18,9 @@ class Application
 
 
 
-    # else
-    #   resp.write "Route not found"
-    #   resp.status = 404
+    else
+      resp.write "Route not found"
+      resp.status = 404
      end
 
     resp.finish
