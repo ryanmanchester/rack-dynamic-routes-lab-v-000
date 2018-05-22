d6b828bdf0355f76b759cc089525896b90325957
Ryan Manchester <ryanm17@gmail.com>
2018-05-22 00:45:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,8 @@ class Application
     if req.path.match(/items/)
       binding.pry
       item_name = req.path.split("/items/").last
-      item = @@items.each do |i|
-        binding.pry
+      item = @@items.find{|i| i.name == item_name}
+      resp.write item.price
       end
     else
       resp.write "Route not found"
